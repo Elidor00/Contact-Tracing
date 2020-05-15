dd78738074b30469b062b6ff3720ceec34bc7275
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-05-15 23:11:42 +0200

    a modest proposal with closures
    wannabe authoritative

--- src/ospedale.erl
+++ src/ospedale.erl
@@ -8,13 +8,23 @@ test(Probability) ->
       case Probability() of
         1 -> PID ! positive;
         _ -> PID ! negative
-      end, 
+      end,
       test(Probability)
   end.
 
+%% TODO move in utils
+make_probability(x) ->
+  fun () ->
+    case (rand:uniform(100) =< x) of
+      true -> 1;
+      false -> 0
+    end
+  end.
+
+
 start() ->
-  io:format("Io sono l'ospedale~n",[]),
-  global:register_name(ospedale, self()),
-  Server = global:whereis_name(server),
-  Server ! {ciao,da,ospedale},
-  test(fun() -> rand:uniform(4) end).
+io:format("Io sono l'ospedale~n", []),
+global:register_name(ospedale, self()),
+Server = global:whereis_name(server),
+Server ! {ciao, da, ospedale},
+test(make_probability(25)).
