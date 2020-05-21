adce36223b52c4e6b0cf5e80aba74feafce620f0
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-05-21 17:24:40 +0200

    Removed comments

--- src/utenti.erl
+++ src/utenti.erl
@@ -1,31 +1,32 @@
 -module(utenti).
--export([main/0, list/2, check_list/2, visit_places/2, contact_tracing/1, actorDispatcher/0, get_places_updates/2, require_test/2, utente/0, start/0]).
--import(utils, [sleep/1, set_subtract/2, take_random/2, check_service/1, make_probability/1]).
+-export([main/0, list_handler/2, check_list/2, visit_place/2, trace_contact/1, actorDispatcher/0, get_places_update/2, require_test/2, utente/0, start/0]).
+-import(utils, [sleep/1, set_subtract/2, take_random/2, check_service/1, make_probability/1, flush/0]).
 
-% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (a)
+
+%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (a) %%%%%%%%%%%%%%%%%%%%
 main() ->
   PidServer = check_service(server),
-  PidServer ! {ciao,da,utente,self()},
+  PidServer ! {ciao, da, utente, self()},
   link(PidServer),
   actorDispatcher().
 
 actorDispatcher() ->
   process_flag(trap_exit, true),
-  ActorList = spawn_link(?MODULE, list, [self(), []]),
-  io:format("[ActorDispatcher] User ~p actorList ~p ~n", [self(), ActorList]),
-  CheckList = spawn_link(?MODULE, check_list, [ActorList, self()]),
-  io:format("[ActorDispatcher] User ~p checklist ~p ~n", [self(), CheckList]),
-  VisitPlace = spawn_link(?MODULE, visit_places, [ActorList, self()]),
-  io:format("[ActorDispatcher] User ~p visitplace ~p ~n", [self(), VisitPlace]),
-  ActorContactTrace = spawn_link(?MODULE, contact_tracing, [self()]),
-  io:format("[ActorDispatcher] User ~p actorContactTrace ~p ~n", [self(), ActorContactTrace]),
-  ActorRequiredTest = spawn_link(?MODULE, require_test, [self(), make_probability(25)]),
-  io:format("[ActorDispatcher] User ~p ActorRequiredTest ~p ~n", [self(), ActorRequiredTest]),
-  ActorMergeList = spawn_link(?MODULE, get_places_updates, [self(), ActorList]),
-  io:format("[ActorDispatcher] User ~p ActorMergeList ~p ~n", [self(), ActorMergeList]),
-  loop(ActorContactTrace, ActorRequiredTest, ActorMergeList, VisitPlace).
+  ActorListHandler = spawn_link(?MODULE, list_handler, [self(), []]),
+  io:format("[ActorDispatcher] User ~p ActorListHandler ~p ~n", [self(), ActorListHandler]),
+  CheckList = spawn_link(?MODULE, check_list, [ActorListHandler, self()]),
+  io:format("[ActorDispatcher] User ~p CheckList ~p ~n", [self(), CheckList]),
+  ActorVisitPlace = spawn_link(?MODULE, visit_place, [ActorListHandler, self()]),
+  io:format("[ActorDispatcher] User ~p ActorVisitPlace ~p ~n", [self(), ActorVisitPlace]),
+  ActorTraceContact = spawn_link(?MODULE, trace_contact, [self()]),
+  io:format("[ActorDispatcher] User ~p ActorTraceContact ~p ~n", [self(), ActorTraceContact]),
+  ActorRequireTest = spawn_link(?MODULE, require_test, [self(), make_probability(25)]),
+  io:format("[ActorDispatcher] User ~p ActorRequireTest ~p ~n", [self(), ActorRequireTest]),
+  ActorGetPlacesUpdate = spawn_link(?MODULE, get_places_update, [self(), ActorListHandler]),
+  io:format("[ActorDispatcher] User ~p ActorGetPlacesUpdate ~p ~n", [self(), ActorGetPlacesUpdate]),
+  dispatcher_loop(ActorTraceContact, ActorRequireTest, ActorGetPlacesUpdate, ActorVisitPlace).
 
-loop(ContactTrace, RequiredTest, MergList, VisitPlace) ->
+dispatcher_loop(ContactTrace, RequiredTest, MergList, VisitPlace) ->
   receive
     {places, PIDLIST} ->
       MergList ! {places, PIDLIST};
@@ -33,36 +34,36 @@ loop(ContactTrace, RequiredTest, MergList, VisitPlace) ->
     positive -> RequiredTest ! positive;
     negative -> RequiredTest ! negative;
     end_visit -> VisitPlace ! end_visit;
-    {'EXIT', Sender, R} -> 
-    io:format("[Dispatcher] ~p received exit from ~p with: ~p~n", [self(), Sender, R]), 
-    exit(R);
+    {'EXIT', Sender, R} ->
+      io:format("[Dispatcher] ~p received exit from ~p with: ~p~n", [self(), Sender, R]),
+      exit(R);
     Msg ->
       % Check unexpected message from other actors
       io:format("[Dispatcher] ~p  ~p~n", [self(), Msg])
   end,
-  loop(ContactTrace, RequiredTest, MergList, VisitPlace).
+  dispatcher_loop(ContactTrace, RequiredTest, MergList, VisitPlace).
 
-% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (b)
-list(PidDispatcher, L) ->
+%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (b) %%%%%%%%%%%%%%%%%%%%
+list_handler(PidDispatcher, L) ->
   receive
     {get_list, Pid} ->
       Pid ! L,
-      list(PidDispatcher, L);
+      list_handler(PidDispatcher, L);
     {update_list, L1} ->
       % monitor all places in L1
       [monitor(process, X) || X <- L1],
-      list(PidDispatcher, L1 ++ L);
-      % messages from a dead place (DOWN)
-    {_, _, process, _, _} ->
+      list_handler(PidDispatcher, L1 ++ L);
+  % messages from a dead place (DOWN)
+    {_, _, process, Pid, _} ->
       global:send(server, {get_places, PidDispatcher}),
-      list(PidDispatcher, L);
+      list_handler(PidDispatcher, set_subtract(L,[Pid]));
     Msg ->
       % Check unexpected message from other actors
       io:format("[User] ~p Messaggio non gestito ~p~n", [self(), Msg]),
-      list(PidDispatcher, L)
+      list_handler(PidDispatcher, L)
   end.
 
-get_places_updates(PidDispatcher, ActorList) ->
+get_places_update(PidDispatcher, ActorList) ->
   receive
     {places, PIDLIST} ->
       ActorList ! {get_list, self()},
@@ -77,13 +78,14 @@ get_places_updates(PidDispatcher, ActorList) ->
           end
       end
   end,
-  get_places_updates(PidDispatcher, ActorList).
+  get_places_update(PidDispatcher, ActorList).
 
-% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (c, d)
+%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (c, d) %%%%%%%%%%%%%%%%%%%%
 check_list(ActorList, PidDispatcher) ->
   ActorList ! {get_list, self()},
   receive
     L ->
+      io:format("Places in ~p check_list: ~p~n", [PidDispatcher, L]),
       case length(L) >= 3 of
         true -> ok;
         false ->
@@ -93,15 +95,8 @@ check_list(ActorList, PidDispatcher) ->
   sleep(10000),
   check_list(ActorList, PidDispatcher).
 
-flush() ->
-  receive
-    _ -> flush()
-  after
-    0 -> ok
-  end.
-
-% PROTOCOLLO DI VISITA DEI LUOGHI
-visit_places(ActorList, PidDispatcher) ->
+%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI VISITA DEI LUOGHI %%%%%%%%%%%%%%%%%%%%
+visit_place(ActorList, PidDispatcher) ->
   ActorList ! {get_list, self()},
   receive
     L ->
@@ -111,24 +106,24 @@ visit_places(ActorList, PidDispatcher) ->
           [LUOGO|_] = take_random(L, 1),
           flush(),
           LUOGO ! {begin_visit, PidDispatcher, REF},
+
           receive end_visit ->
             io:format("Received end_visit"),
             LUOGO ! {end_visit, PidDispatcher, REF}
           after 5000 + rand:uniform(5000) -> ok end,
 
-%%          sleep(5000 + rand:uniform(5000)),
           LUOGO ! {end_visit, PidDispatcher, REF};
         false -> ok
       end,
       sleep(3000 + rand:uniform(2000)),
-      visit_places(ActorList, PidDispatcher)
+      visit_place(ActorList, PidDispatcher)
   end.
 
-% PROTOCOLLO DI RILEVAMENTO DEI CONTATTI (a,b)
-contact_tracing(PidDispatcher) ->
+%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI RILEVAMENTO DEI CONTATTI (a,b) %%%%%%%%%%%%%%%%%%%%
+trace_contact(PidDispatcher) ->
   process_flag(trap_exit, true),
-  contact_tracing_loop(PidDispatcher).
-contact_tracing_loop(PidDispatcher) ->
+  trace_contact_loop(PidDispatcher).
+trace_contact_loop(PidDispatcher) ->
   receive
     {contact, PID} ->
       %let it fail
@@ -138,7 +133,7 @@ contact_tracing_loop(PidDispatcher) ->
       %catch X ->
       %  io:format("[User] ~p unable to link to ~p error ~p~n", [PidDispatcher, PID, X])
       %end,
-      contact_tracing_loop(PidDispatcher);
+      trace_contact_loop(PidDispatcher);
     {'EXIT', _, R} ->
       case R of
         quarantine ->
@@ -147,13 +142,13 @@ contact_tracing_loop(PidDispatcher) ->
         positive ->
           io:format("[User] ~p entro in quaratena ~n", [PidDispatcher]),
           exit(quarantine)
-      end ;
+      end;
     Msg ->
       % Check unexpected message from other actors
       io:format("[User] ~p Messaggio non gestito ~p~n", [self(), Msg])
   end.
 
-% PROTOCOLLO DI TEST (a,b)
+%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI TEST (a,b) %%%%%%%%%%%%%%%%%%%%
 require_test(PidDispatcher, Probability) ->
   sleep(30000),
   case Probability() of
@@ -172,8 +167,8 @@ require_test(PidDispatcher, Probability) ->
   require_test(PidDispatcher, Probability).
 
 utente() ->
-  io:format("Io sono l'utente ~p~n",[self()]),
+  io:format("Io sono l'utente ~p~n", [self()]),
   main().
 
 start() ->
-  [ spawn(fun utente/0) || _ <- lists:seq(1,10) ].
+  [spawn(fun utente/0) || _ <- lists:seq(1, 10)].
